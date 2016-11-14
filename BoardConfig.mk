cac7411a53904899b9ddd78eb6e0ad1b774fab8e
ZIM <zim555@mail.ru>
2016-11-14 14:23:38 +0200

    Add kernel config name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@
 -include device/samsung/msm8916-common/BoardConfigCommon.mk
 
 # Kernel
-TARGET_KERNEL_CONFIG :=
+TARGET_KERNEL_CONFIG      := msm8916_fortunave3g_eur_defconfig
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := fortunave3g,SM-G530H,fortuna3g
+TARGET_OTA_ASSERT_DEVICE  := fortunave3g,SM-G530H,fortuna3g
