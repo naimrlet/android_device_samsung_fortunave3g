4bb1b214bcf58c4734648b56325428a55e63ce7d
ZIM <zim555@mail.ru>
2016-11-14 14:26:19 +0200

    Create cm.mk

---
+++ cm.mk
@@ -0,0 +1,14 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/full_fortunave3g.mk)
+
+PRODUCT_RELEASE_NAME := fortunave3g
+
+# Setup device configuration
+PRODUCT_NAME := cm_fortunave3g
+PRODUCT_DEVICE := fortunave3g
+PRODUCT_BRAND := Samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := SM-G530H
