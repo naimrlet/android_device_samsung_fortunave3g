91f59267187613a44038f853aab80fe680bac8fe
ZIM <zim555@mail.ru>
2016-11-14 13:00:37 +0200

    Begin import BoardConfig

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+# Inherit from common samsung msm8916
+-include device/samsung/msm8916-common/BoardConfigCommon.mk
+
+# Kernel
+TARGET_KERNEL_CONFIG :=
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := fortunave3g,SM-G530H,fortuna3g
