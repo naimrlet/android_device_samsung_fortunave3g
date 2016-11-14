b8b3f8c11f176564833a038eb2d1858a75de860b
ZIM <zim555@mail.ru>
2016-11-14 14:57:55 +0200

    Update path for device.mk

--- full_fortunave3g.mk
+++ full_fortunave3g.mk
@@ -14,7 +14,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fortunave3g device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product, device/samsung/fortunave3g/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_fortunave3g
