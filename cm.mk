04f6c0ed45088c81b845e4d3672968faa4afbab2
ZIM <zim555@mail.ru>
2016-11-14 14:45:58 +0200

    Edit path

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/full_fortunave3g.mk)
+$(call inherit-product, device/samsung/fortunave3g/full_fortunave3g.mk)
 
 # Setup device configuration
 PRODUCT_NAME := cm_fortunave3g
