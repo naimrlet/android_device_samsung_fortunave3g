faa74a7469f456531a3264d2db5fd63d6f0b83cb
ZIM <zim555@mail.ru>
2016-11-15 13:41:06 +0200

    Fix device tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,18 @@
-# Inherit from common samsung msm8916
--include device/samsung/fortuna-common/BoardConfigCommon.mk
+# Inherit from common samsung fortuna
+-include device/samsung/fortunaxx-common/BoardConfigCommon.mk
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE  := fortunave3g,SM-G530H,fortuna3g
 
 # Kernel
 TARGET_KERNEL_CONFIG      := msm8916_fortunave3g_eur_defconfig
+
+
+
+
+
+
+
 
 # inherit from the proprietary version
 -include vendor/samsung/fortunave3g/BoardConfigVendor.mk
