965d8c94230c4788e81f9f4925aef2fa36474338
ZIM <zim555@mail.ru>
2016-11-16 10:13:17 +0200

    Add more configs and use init for this device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,18 +1,18 @@
 # Inherit from common samsung fortuna
 -include device/samsung/fortunaxx-common/BoardConfigCommon.mk
 
+LOCAL_PATH := device/samsung/fortunave3g
+
 # Assert
-TARGET_OTA_ASSERT_DEVICE  := fortunave3g,SM-G530H,fortuna3g
+TARGET_OTA_ASSERT_DEVICE := fortunave3g,SM-G530H,fortuna3g
 
 # Kernel
-TARGET_KERNEL_CONFIG      := msm8916_fortunave3g_eur_defconfig
+TARGET_KERNEL_CONFIG := msm8916_fortunave3g_eur_defconfig
 
-
-
-
-
-
-
+# Vendor Init
+TARGET_UNIFIED_DEVICE        := true
+TARGET_INIT_VENDOR_LIB       := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE  := $(LOCAL_PATH)/init/init_fortunave3g.c
 
 # inherit from the proprietary version
 -include vendor/samsung/fortunave3g/BoardConfigVendor.mk
